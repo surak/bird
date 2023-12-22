b7b9412e9c4bdff97b91b481bf9b2bfef5f8c8f6
Alexandre Strube <surak@surak.eti.br>
2023-12-22 15:14:05 +0100

    Add background process for real-time object detection and servo control

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 source sc_venv_template/activate.sh 
-python real_time_with_labels.py --model=./mobilenet_v2.tflite --label=./coco_labels.txt --target="cell phone"
+python real_time_with_labels.py --model=./mobilenet_v2.tflite --label=./coco_labels.txt --target="cell phone" &
+python ./ServoKitExample.py
