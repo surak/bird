497e9ca364c0e4981520817e6ec72094d016c101
Alexandre Strube <surak@surak.eti.br>
2023-12-22 15:17:45 +0100

    Add comments on background execution for object detection and servo control

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 source sc_venv_template/activate.sh 
+
+# Open it in background as I want to control the servo after the camera is on
 python real_time_with_labels.py --model=./mobilenet_v2.tflite --label=./coco_labels.txt --target="cell phone" &
+
+# Run the servo control script. This messes up output, but it's fine for now
 python ./ServoKitExample.py
